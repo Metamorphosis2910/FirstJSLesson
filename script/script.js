a48944caf553fefcf04fb97f9e1089c01bc673e9
Arthur <ggurvon@gmail.com>
2021-02-18 19:26:42 +0500

    first

---
+++ script/script.js
@@ -0,0 +1,36 @@
+var name; 
+var name = prompt('Введите своё имя');
+console.log(name);
+
+var age;
+var age = +prompt('Введите возраст')
+console.log(age);
+
+var plus;
+var plus = prompt('Сколько будет 12+15?')
+console.log('Ваш ответ ' + plus);
+console.log('Правильный ответ = ' + (12+15));
+
+var minus;
+var minus = prompt('Сколько будет 27-15?')
+console.log('Ваш ответ ' + minus);
+console.log('Правильный ответ = ' + (27-15));
+
+var umnojeniye;
+var umnojeniye = prompt('Сколько будет 8*6?')
+console.log('Ваш ответ ' + umnojeniye);
+console.log('Правильный ответ = ' + (8*6));
+
+var deleniye;
+var deleniye = prompt('Сколько будет 56/4')
+console.log('Ваш ответ ' + deleniye);
+console.log('Правильный ответ = ' + (56/4));
+
+var deleniyeOstatok 
+var deleniyeOstatok = prompt('Сколько будет 59%3?')
+console.log('Ваш ответ ' + deleniyeOstatok);
+console.log('Правильный ответ = ' + (59%3));
+
+var console;
+var console = ('Теперь пожалуйста зайдите в консоль и проверьте ваши ответы') 
+alert(console)
